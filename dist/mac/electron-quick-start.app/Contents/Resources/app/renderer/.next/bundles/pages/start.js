e5b014813f3ab28174fd1731313bd87a94c45758
EricEisaman <eeisaman@auburnschl.edu>
2017-07-17 13:47:04 -0400

    First Commit

---
+++ dist/mac/electron-quick-start.app/Contents/Resources/app/renderer/.next/bundles/pages/start.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/start', function() {
+            var comp = module.exports=webpackJsonp([2],[],[360]);
+            return { page: comp.default }
+          })
+        
