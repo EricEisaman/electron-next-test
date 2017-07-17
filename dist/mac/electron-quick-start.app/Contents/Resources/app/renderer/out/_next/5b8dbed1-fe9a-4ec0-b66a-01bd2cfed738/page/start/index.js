27a4b21309311c38d96805a7dc26134ae865066b
EricEisaman <eeisaman@auburnschl.edu>
2017-07-17 14:57:36 -0400

    ignore dist/

--- dist/mac/electron-quick-start.app/Contents/Resources/app/renderer/out/_next/5b8dbed1-fe9a-4ec0-b66a-01bd2cfed738/page/start/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/start', function() {
-            var comp = module.exports=webpackJsonp([2],[],[360]);
-            return { page: comp.default }
-          })
-        
