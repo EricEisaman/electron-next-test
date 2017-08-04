b867a85df9ada082f050786f67aee713c2eadc73
EricEisaman <eeisaman@auburnschl.edu>
2017-08-04 17:17:01 -0400

    clean

--- renderer/.next/bundles/pages/start.js
+++ renderer/.next/bundles/pages/start.js
@@ -1,6 +1,73 @@
 
           window.__NEXT_REGISTER_PAGE('/start', function() {
-            var comp = module.exports=webpackJsonp([2],[],[360]);
+            var comp = module.exports =
+webpackJsonp([5],{
+
+/***/ 556:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(557);
+
+
+/***/ }),
+
+/***/ 557:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(__resourceQuery) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(11);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/algo/Documents/gameDev/electron/electron-next-test/renderer/pages/start.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, "This is Next.js speaking!");
+};
+
+ ;(function register() { /* react-hot-loader/webpack */ if (true) { if (typeof __REACT_HOT_LOADER__ === 'undefined') { return; } if (typeof module.exports === 'function') { __REACT_HOT_LOADER__.register(module.exports, 'module.exports', "/Users/algo/Documents/gameDev/electron/electron-next-test/renderer/pages/start.js"); return; } for (var key in module.exports) { if (!Object.prototype.hasOwnProperty.call(module.exports, key)) { continue; } var namedExport = void 0; try { namedExport = module.exports[key]; } catch (err) { continue; } __REACT_HOT_LOADER__.register(namedExport, key, "/Users/algo/Documents/gameDev/electron/electron-next-test/renderer/pages/start.js"); } } })();
+    (function (Component, route) {
+      if (false) return
+      if (false) return
+
+      var qs = __webpack_require__(70)
+      var params = qs.parse(__resourceQuery.slice(1))
+      if (params.entry == null) return
+
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/start")
+  
+/* WEBPACK VAR INJECTION */}.call(exports, "?entry"))
+
+/***/ })
+
+},[556]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlcy9wYWdlcy9zdGFydC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3BhZ2VzL3N0YXJ0LmpzPzQ1YTFiZmIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgKCkgPT4gKFxuICA8c3Bhbj5UaGlzIGlzIE5leHQuanMgc3BlYWtpbmchPC9zcGFuPlxuKVxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL3BhZ2VzL3N0YXJ0LmpzP2VudHJ5Il0sIm1hcHBpbmdzIjoiO0E7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUE7QUFDQTs7Ozs7OztBQURBO0FBQ0E7O0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFEQTtBQUNBOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBIiwic291cmNlUm9vdCI6IiJ9
             return { page: comp.default }
           })
         
--- renderer/.next/dist/pages/start.js
+++ renderer/.next/dist/pages/start.js
@@ -10,6 +10,13 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var _jsxFileName = "/Users/algo/Documents/gameDev/electron/electron-next-test/renderer/pages/start.js?entry";
+
 exports.default = function () {
-  return _react2.default.createElement("span", null, "This is Next.js speaking!");
+  return _react2.default.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, "This is Next.js speaking!");
 };
